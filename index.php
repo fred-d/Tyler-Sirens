2eeba5c920119149b818ac61db8bc2779ba73bfc
mofujofu <j.marmar47@gmail.com>
2015-10-12 13:56:58 -0500

    Hello, World!

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(-1);
+
 require_once 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+$container = new \Slim\Container;
 
-$app->get('/', function() use ($app) {
-    $app->render("lib/views/home.view.php",
-        array(
-            'message' => 'Hello, World!'
-        ));
+// Register component on container
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('/lib/templates');
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $c['router'],
+        $c['request']->getUri()
+    ));
+
+    return $view;
+};
+
+$app = new \Slim\App($container);
+//$app->config('debug', true);
+
+$app->get('/', function($request, $response, $args){
+    echo 'Hello, World!';
 });
 
+// Render Twig template in route
+$app->get('/home', function ($request, $response, $args) {
+    return require_once("lib/templates/home.html");
+});
+
+// Run app
 $app->run();
